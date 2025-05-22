4cda1516eab9dba99327c22f7f9f0761f48bc6c4
cristian <cristiantrevelin@hotmail.com>
2025-05-22 15:56:51 -0300

    feat: colocando programa em loop

--- calculadora.c
+++ calculadora.c
@@ -12,7 +12,7 @@ int menu()
     printf("\n[0] Sair.");
     printf("\n----------------------------------------------");
 
-    printf("\n>> Selecione uma opção..: ");
+    printf("\n> Selecione uma opção..: ");
     scanf("%d", &opcao);
 
     if (opcao < 0 || opcao > 4)
@@ -23,5 +23,30 @@ int menu()
 
 void main()
 {
+    int opcao, calculadora_ativa = 1;
 
+    while (calculadora_ativa)
+    {
+        do
+        {
+            opcao = menu();
+
+            if (opcao == -1)
+                printf("\n");
+
+        } while(opcao == -1);
+
+        if (opcao == 0)
+        {
+            printf("\n>> Programa encerrado.");
+            calculadora_ativa = 0;
+        }
+
+        switch(opcao)
+        {
+            case 1:
+
+                break;
+        }
+    }
 }
