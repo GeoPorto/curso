e7ee1e50fdf90fb9d1679d1f23504d8a92120b26
Geovane Souza <geovane.g1000@gmail.com>
2025-05-22 16:00:13 -0300

    commit calculadora

--- calculadora.c
+++ calculadora.c
@@ -9,17 +9,68 @@ int subtracao(int num1, int num2)
 {
     return num1 - num2;
 }
+int multiplicacao(int num1, int num2)
+{
+    return num1 * num2;
+}
 
+int divisao(int num1, int num2)
+{
+    return num1 / num2;
+}
 void main()
 {
     int opc;
+    int num1, num2;
 
     do {
-
         printf("Soma - 1\n");
         printf("Subtracao - 2\n");
-        printf("Sair - 0\n");
+        printf("multiplicacao - 3\n");
+        printf("divisao - 4\n");
+        printf("Sair - 5\n");
+        printf("opc:");
+        scanf("%d",&opc);
 
-    }while(opc != 0);
+        switch(opc)
+        {
+        case 1:
+        printf("primeiro numero:");
+        scanf("%d",&num1);
+        printf("\nsegundo numero:");
+        scanf("%d",&num2);
+        printf("\nresultado: %d\n", soma(num1, num2));
+
+        break;
+
+        case 2:
+        printf("primeiro numero:");
+        scanf("%d",&num1);
+        printf("\nsegundo numero:");
+        scanf("%d",&num2);
+        printf("\nresultado: %d\n", subtracao(num1, num2));
+
+        break;
+
+        case 3:
+        printf("primeiro numero:");
+        scanf("%d",&num1);
+        printf("\nsegundo numero:");
+        scanf("%d",&num2);
+        printf("\nresultado: %d\n", multiplicacao(num1, num2));
+
+        break;
+
+        case 4:
+        printf("primeiro numero:");
+        scanf("%d",&num1);
+        printf("\nsegundo numero:");
+        scanf("%d",&num2);
+        printf("\nresultado: %d\n", divisao(num1, num2));
+
+        break;
+        }
+
+    }while(opc != 5);
 
 }
