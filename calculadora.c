a0700bbc3c674964b9d2095673bb5f021610116a
cristian <cristiantrevelin@hotmail.com>
2025-05-22 15:41:11 -0300

    feat: criando função 'menu'

--- calculadora.c
+++ calculadora.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
+int menu()
+{
+    int opcao;
+
+    printf("[CALCULADORA MENU]: --------------------------");
+    printf("\n[1] Adição;");
+    printf("\n[2] Subtração;");
+    printf("\n[3] Multiplicação;");
+    printf("\n[4] Divisão;");
+    printf("\n[0] Sair.");
+    printf("\n----------------------------------------------");
+
+    printf("\n>> Selecione uma opção..: ");
+    scanf("%d", &opcao);
+
+    if (opcao < 0 || opcao > 4)
+        return -1;
+
+    return opcao;
+}
+
 void main()
 {
 
